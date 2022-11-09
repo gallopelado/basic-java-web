694bf2494858f17a3a2446b4a4c31b34836b822d
gallopelado <juanftp100@gmail.com>
2022-11-09 14:39:50 -0300

    add: first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+### Using basic java web on vscode
+#### The purpose is to study jsp and servlets without any IDE like netbeans, eclipse.
---
+++ app/src/main/java/api/ApiServlet.java
@@ -0,0 +1,23 @@
+package api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ApiServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("./api works. Try ./api/getMessage");
+
+    }
+}
